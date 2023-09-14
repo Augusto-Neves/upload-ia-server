1315490ad73814d8f1d002b5cfc7555bb2442000
Augusto Neves <augustoneves@frwk.com.br>
2023-09-14 16:52:09 -0300

    creating routes

---
+++ src/routes/upload-video.ts
@@ -0,0 +1,57 @@
+import fastifyMultipart from '@fastify/multipart';
+import path from 'path';
+import fs from 'fs';
+import { FastifyInstance } from 'fastify';
+import { prisma } from '../lib/prisma';
+import { randomUUID } from 'crypto';
+import { pipeline } from 'stream';
+import { promisify } from 'node:util';
+
+const pump = promisify(pipeline);
+
+export async function uploadVideoRoute(app: FastifyInstance) {
+  app.register(fastifyMultipart, {
+    limits: {
+      fieldSize: 1048576 * 25, //25mb
+    },
+  });
+  
+  app.post('/videos', async (req, res) => {
+    const data = await req.file();
+
+    if (!data) {
+      return res.status(400).send({ error: 'Missing file input.' });
+    }
+
+    const extension = path.extname(data.filename);
+
+    if (extension !== '.mp3') {
+      return res
+        .status(400)
+        .send({ error: 'Invalid input type, please upload a mp3 file.' });
+    }
+
+    const fileBaseName = path.basename(data.filename, extension);
+
+    const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`;
+
+    const uploadDestination = path.resolve(
+      __dirname,
+      '../../tmp',
+      fileUploadName
+    );
+
+    await pump(data.file, fs.createWriteStream(uploadDestination));
+
+    const video = await prisma.video.create({
+      data: {
+        name: data.filename,
+        path: uploadDestination,
+      },
+    });
+
+    return {
+      video,
+    };
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors';
+import { getAllPromptsRoute } from './routes/get-all-prompts';
+import { uploadVideoRoute } from './routes/upload-video';
+import { createTranscriptionsRoute } from './routes/create-transcription';
+import { generateAICompletionRoute } from './routes/genarate-ai-completion';
 
 const app = fastify();
 
-app.get('/', (_req, res) => {
-  res.send('Hello world');
+app.register(fastifyCors, {
+  origin: '*',
 });
+
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionsRoute);
+app.register(generateAICompletionRoute);
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server is running at port 3333');
